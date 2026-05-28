b912eb0c011be887ba528f66dd1073998cdd6b10
azizbekqiyomov5555-arch <azizbekqiyomov5555@gmail.com>
2026-05-28 09:46:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,8 +31,8 @@ from telegram.ext import (
 BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8808903920:AAFfmqUyvOKcykhYVRWt5Gcd0kreYcTS3wk"
 ADMIN_ID   = int(os.environ.get("ADMIN_ID") or "5944555300")
 
-JSONBLOB_URL      = os.environ.get("JSONBLOB_URL") or "https://jsonblob.com/api/jsonBlob/019e2acf-74a4-7dad-9ad9-5a33490caa9b"
-GSHEET_ID         = os.environ.get("GSHEET_ID")    or "1XBuO-K6Aqa6ocISRrAKO7pi8Fhl0YJVkcnQSGeYCqsQ"
+JSONBLOB_URL      = os.environ.get("JSONBLOB_URL") or ""
+GSHEET_ID         = os.environ.get("GSHEET_ID")    or ""
 GSHEET_API        = os.environ.get("GSHEET_API")   or ""
 NPOINT_URL        = os.environ.get("NPOINT_URL")   or ""
 LOCAL_BACKUP_FILE = "db_backup.json"
